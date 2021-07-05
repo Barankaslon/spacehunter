eefb360294848b441736be5f8fda8cce26f9053b
Alex <barankaslon@hotmail.com>
2021-07-05 00:10:50 -0300

    SetUp Project

---
+++ SpaceHunter/Game.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Player.h"
+
+
+class Game
+{
+	private:
+		RenderWindow* window;
+public:
+	Game(RenderWindow *window);
+	virtual ~Game();
+
+	//Accessors
+	inline RenderWindow& getWindow() { return *this->window; }
+
+	//Setters
+
+
+	//Functions
+	void Update();
+	void Draw();
+};
+
---
+++ SpaceHunter/main.cpp
@@ -0,0 +1,25 @@
+#include "Game.h"
+
+int main()
+{
+
+	RenderWindow window(VideoMode(800, 600), "Space Hunter Game", Style::Default);
+
+	window.setFramerateLimit(60);
+
+	//Game Loop
+
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+			if (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape)
+				window.close();
+		}
+	}
+
+	return 0;
+}
